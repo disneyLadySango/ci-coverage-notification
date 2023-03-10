f5d998453e0309353bec5020c73416e719332420
disneyLadySango <sangota.d.duck@gmail.com>
2023-03-10 15:13:40 +0900

    feat: サンプルの実装を追加

---
+++ src/utils/sum.ts
@@ -0,0 +1,7 @@
+export const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+export const sums = (...args: number[]): number => {
+  return args.reduce((prev, current) => prev + current, 1);
+};
